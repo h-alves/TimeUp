f1040f9a1b4d368e7ba508359e58035e4c096e93
Henrique Alves <henriqueasemmer@gmail.com>
2023-08-01 22:45:39 -0300

    Initial Commit

---
+++ Time's Up/Time_s_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_s_UpApp.swift
+//  Time's Up
+//
+//  Created by Henrique Semmer on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Time_s_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
